442d7fbe8616cea4c332aa4b9bda4635eb6a568d
Daniel Balcarek <Gramli@seznam.cz>
2020-11-25 15:59:35 +0100

    Edit desing

--- ScriperSol/Scriper/ViewModels/AboutVM.cs
+++ ScriperSol/Scriper/ViewModels/AboutVM.cs
@@ -5,6 +5,6 @@
         public string Author => "Author: Gramli";
         public string Version => $"Version: {GetType().Assembly.GetName().Version}";
         public string Text =>
-            @$"Scriper was developed by using lot of frameworks and libraries, you can find their list on Github (https://github.com/Gramli/Scriper). All icons are created by Icons8 (https://icons8.com/)";
+            @$"Scriper was developed by using lot of frameworks and libraries, you can find their list on Github (https://github.com/Gramli/Scriper). All icons are created by Icons8 (https://icons8.com/). If you like Scriper share it :)";
     }
 }

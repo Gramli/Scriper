9bf919c9b1f708f3b83805d95a0244b7a27a347c
Daniel Balcarek <daniel.balcarek@tieto.com>
2021-06-28 15:07:34 +0200

    fix unit tests for TimeScheduler

--- ScriperSol/Scriper.UnitTests/TimeScheduleTests.cs
+++ ScriperSol/Scriper.UnitTests/TimeScheduleTests.cs
@@ -3,78 +3,28 @@ using ScriperLib;
 using ScriperLib.Configuration;
 using ScriperLib.ScriptScheduler;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Win32.TaskScheduler;
 
 namespace Scriper.UnitTests
 {
     public class TimeScheduleTests : TestsBase
     {
         [Test]
-        public void Add()
+        public void AddAndRemove()
         {
             var scriperLibContainer = new ScriperLibContainer(filePath);
             var scriptConfig = scriperLibContainer.GetInstance<IScriptConfiguration>();
+            scriptConfig.Name = "myScript";
             var timeScheduleConfig = scriperLibContainer.GetInstance<ITimeTriggerConfiguration>();
-
             timeScheduleConfig.Time = DateTime.Now.AddMinutes(10);
             timeScheduleConfig.ScriptTriggerType = ScriperLib.Enums.ScriptTriggerType.Time;
 
             scriptConfig.TimeScheduleConfigurations.Add(timeScheduleConfig);
 
             var scriptScheduleManager = scriperLibContainer.GetInstance<IScriptSchedulerManager>();
-            scriptScheduleManager.Add("","", scriptConfig);
+            scriptScheduleManager.Add("something","something", scriptConfig);
 
-            //Check Added
-
-            //Remove at end
-        }
-
-        [Test]
-        public void Get()
-        {
-            var scriperLibContainer = new ScriperLibContainer(filePath);
-            var scriptConfig = scriperLibContainer.GetInstance<IScriptConfiguration>();
-            var timeScheduleConfig = scriperLibContainer.GetInstance<ITimeTriggerConfiguration>();
-
-            timeScheduleConfig.Time = DateTime.Now.AddMinutes(10);
-            timeScheduleConfig.ScriptTriggerType = ScriperLib.Enums.ScriptTriggerType.Time;
-
-            scriptConfig.TimeScheduleConfigurations.Add(timeScheduleConfig);
-
-            var scriptScheduleManager = scriperLibContainer.GetInstance<IScriptSchedulerManager>();
-            scriptScheduleManager.Add("", "", scriptConfig);
-
-
-            //var timeScheduleConfigLoaded = scriptScheduleManager.Get(scriptConfig.Name);
-
-            //Assert.Equals(timeScheduleConfig.Time, timeScheduleConfigLoaded.Time);
-            //Assert.Equals(timeScheduleConfig.ScriptTriggerType, timeScheduleConfigLoaded.ScriptTriggerType);
-
-            //Remove at end
-        }
-
-        [Test]
-        public void Remove()
-        {
-            var scriperLibContainer = new ScriperLibContainer(filePath);
-            var scriptConfig = scriperLibContainer.GetInstance<IScriptConfiguration>();
-            var timeScheduleConfig = scriperLibContainer.GetInstance<ITimeTriggerConfiguration>();
-
-            timeScheduleConfig.Time = DateTime.Now.AddMinutes(10);
-            timeScheduleConfig.ScriptTriggerType = ScriperLib.Enums.ScriptTriggerType.Time;
-
-            scriptConfig.TimeScheduleConfigurations.Add(timeScheduleConfig);
-
-            var scriptScheduleManager = scriperLibContainer.GetInstance<IScriptSchedulerManager>();
-            scriptScheduleManager.Add("", "", scriptConfig);
-
-            //Check Added
-
-            scriptScheduleManager.Remove(scriptConfig.Name);
-
-            //Check Removed
-            //Remove at end
+            TaskService.Instance.RootFolder.DeleteTask(scriptConfig.Name);
         }
     }
 }

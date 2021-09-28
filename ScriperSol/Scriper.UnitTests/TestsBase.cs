38355a148e413d97ea5e49d581901c77613f83fa
Daniel Balcárek <Gramli@seznam.cz>
2021-09-28 22:30:35 +0200

    Feature/nuget update (#53)
    * Change version to .NET5 & Update Nuget Packages
    
    * pre-pust automatic commit for versioning

--- ScriperSol/Scriper.UnitTests/TestsBase.cs
+++ ScriperSol/Scriper.UnitTests/TestsBase.cs
@@ -3,6 +3,6 @@
     public abstract class TestsBase
     {
         protected readonly string filePath = @"Assets\defaultScriper.config";
-        protected readonly string uiFilePath = @"C:\Github\Scriper\ScriperSol\Scriper.UnitTests\Assets\ScriperUI.config";
+        protected readonly string uiFilePath = @"Assets\ScriperUI.config";
     }
 }

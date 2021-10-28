0e1548db2cec126fe59ce0ccf1870d6cac20f52b
Daniel Balcárek <Gramli@seznam.cz>
2021-10-28 22:55:11 +0200

    Bug/power shell arguments (#71)
    * fix: powershell parameters parsing
    
    * pre-pust automatic commit for versioning
    
    * fix: codefactor issues
    
    * pre-pust automatic commit for versioning

--- ScriperSol/ScriperLib/Arguments/ArgumentsSplitter.cs
+++ ScriperSol/ScriperLib/Arguments/ArgumentsSplitter.cs
@@ -66,7 +66,7 @@ namespace ScriperLib.Arguments
             {
                 var character = arguments[i];
 
-                if(character == '"')
+                if(character == '"' || character == '\'')
                 {
                     isEscape = !isEscape;
                     continue;
